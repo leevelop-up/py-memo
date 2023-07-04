ade7bfdad3bf26dcbdf7af03cb7fa55b57a53bba
leevelop <merong0075@gmail.com>
2023-07-05 00:09:02 +0900

    python curd2

--- main.py
+++ main.py
@@ -23,4 +23,22 @@ def read_memo():
     return memos
 
 
+@app.put("/memos/{memo_id}")
+def put_memo(req_memo: Memo):
+    for memo in memos:
+        if memo.id == req_memo.id:
+            memo.content = req_memo.content
+            return "성공"
+    return "실패"
+
+
+@app.delete("/memos/{memo_id}")
+def delete_memo(memo_id):
+    for idx, memo in enumerate(memos):
+        if memo.id == memo_id:
+            memos.pop(idx)
+            return "성공"
+    return "실패"
+
+
 app.mount("/", StaticFiles(directory="static", html=True), name="static")
--- static/app.js
+++ static/app.js
@@ -1,7 +1,43 @@
+async function editMemo(event) {
+  const id = event.target.dataset.id;
+  const editinput = prompt("수정값 입력");
+  const rest = await fetch(`/memos/${id}`, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      id,
+      content: editinput,
+    }),
+  });
+  readMemo();
+}
+
+async function deleteBtn(event) {
+  const id = event.target.dataset.id;
+  const rest = await fetch(`/memos/${id}`, {
+    method: "DELETE",
+  });
+  readMemo();
+}
+
 function displayMemos(memo) {
   const ul = document.querySelector("#memo-ul");
   const li = document.createElement("li");
-  li.innerText = `[id:${memo.id}] ${memo.content}`;
+  li.innerText = `${memo.content}`;
+  const editBtn = document.createElement("button");
+  editBtn.innerText = "수정";
+  editBtn.addEventListener("click", editMemo);
+  editBtn.dataset.id = memo.id;
+
+  const delBtn = document.createElement("button");
+  delBtn.innerText = "삭제";
+  delBtn.addEventListener("click", deleteBtn);
+  delBtn.dataset.id = memo.id;
+
+  li.appendChild(editBtn);
+  li.appendChild(delBtn);
   ul.appendChild(li);
 }
 
